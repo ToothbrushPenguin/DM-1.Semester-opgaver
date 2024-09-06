4526d00cca25413b8bd6a7c4fd5a05fcbebe0d95
ToothbrushPenguin <emil.k.m@hotmail.dk>
2024-09-06 18:47:17 +0200

    add 2 exesise

---
+++ Pr02_Conditionals/Øvelse3/Øvelse3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Øvelse3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, World!");
+            string tee = Console.ReadLine();
+            Console.WriteLine("Hello, World!");
+            int test = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
