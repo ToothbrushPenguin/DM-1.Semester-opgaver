db27467cb0d154afaf1ae854c760cec4aa86743e
ToothbrushPenguin <emil.k.m@hotmail.dk>
2024-09-07 10:25:06 +0200

    added øvelse4 and edited øvelse3

--- Pr02_Conditionals/Øvelse3/Øvelse3/Program.cs
+++ Pr02_Conditionals/Øvelse3/Øvelse3/Program.cs
@@ -4,11 +4,41 @@
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello, World!");
-            string tee = Console.ReadLine();
-            Console.WriteLine("Hello, World!");
-            int test = int.Parse(Console.ReadLine());
+
+            Console.Write("Navn: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Alder: ");
+            int age = int.Parse(Console.ReadLine());
+
+            string beskrivelse = "";
+
+            if (age >= 0 && age <= 12)
+            {
+                beskrivelse = "et barn";
+            }
+            else if (age >= 13 && age <= 19)
+            {
+                beskrivelse = "en teenager";
+
+            }
+            else if (age >= 20 && age <= 25)
+            {
+                beskrivelse = "en studerende";
+            }
+            else if (age >= 26 && age <= 67)
+            {
+                beskrivelse = "i arbejde";
+            }
+            else if (age >= 67)
+            {
+                beskrivelse = "pensionist";
+            }
+
+
+            Console.Write(name + " er " + age + " år gammel og er " + beskrivelse);
+
+
 
         }
     }
