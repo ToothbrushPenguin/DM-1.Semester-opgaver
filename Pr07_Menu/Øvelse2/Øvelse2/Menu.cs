1ce73776d9bf836bb513fb4fa158a611a9c45007
ToothbrushPenguin <emil.k.m@hotmail.dk>
2024-09-19 15:59:35 +0200

    file 19-09-2024

---
+++ Pr07_Menu/Øvelse2/Øvelse2/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Øvelse2
+{
+    public class Menu
+    {
+        public string title;
+
+        private MenuItem[] menuItems = new MenuItem[100];
+
+        private int itemCount = 0;
+
+        public Menu(string ti)
+        {
+            title = ti;
+        }
+
+        public void AddMenuItem(string menuTitle)
+        {
+            MenuItem mi = new MenuItem(menuTitle);
+            menuItems[itemCount] = mi;
+            itemCount++;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(title);
+            for (int i = 0; i < itemCount; i++)
+            {
+                    Console.WriteLine(menuItems[i].title);
+            }
+            Console.WriteLine("(Tryk 'mellrum' eller '0' for at afslutte)");
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Pr07_Menu/Øvelse2/Øvelse2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Øvelse2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Menu mainMenu = new Menu("Min fantastiske menu");
+
+
+            // First menu item
+            mainMenu.AddMenuItem("1. Gør dit");
+            // Second menu item
+            mainMenu.AddMenuItem("2. Gør dat");
+            // Third menu item
+            mainMenu.AddMenuItem("3. Gør noget");
+            // Last menu item
+            mainMenu.AddMenuItem("4. Få svaret på livet, universet og alting");
+            
+
+            mainMenu.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
