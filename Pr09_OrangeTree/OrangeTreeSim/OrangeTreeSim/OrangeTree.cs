20fe3b1863beba4ee1cba20d8aaf7ba53f1f6830
ToothbrushPenguin <emil.k.m@hotmail.dk>
2024-09-25 18:38:03 +0200

    25092024 added start files

---
+++ Pr09_OrangeTree/OrangeTreeSim/OrangeTreeSim/OrangeTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeTreeSim
+{
+    class OrangeTree
+    {
+        private int age;
+        private int height;
+        private bool treeAlive;
+        private int numOranges;
+        private int orangesEaten;
+
+        public void SetAge(int age) { this.age = age; }
+        public int GetAge() { return age; }
+        public void SetHeight(int height) { this.height = height; }
+        public int GetHeight() { return height; }
+        public void SetTreeAlive(bool treeAlive){ this.treeAlive = treeAlive; }
+        public bool GetTreeAlive() { return treeAlive; }
+        public int GetNumOranges(){ return numOranges; }
+        public int GetOrangesEaten(){ return orangesEaten; }
+
+        public void OneYearPasses()
+        {
+
+        }
+
+        public void EatOrange(int count)
+        {
+
+        }
+    }
+}
---
+++ Pr09_OrangeTree/OrangeTreeSim/OrangeTreeTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace OrangeTreeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
