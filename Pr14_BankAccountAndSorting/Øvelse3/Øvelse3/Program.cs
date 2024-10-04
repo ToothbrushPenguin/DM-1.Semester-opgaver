0e69fc595273e2b11442338c5efee05c6e78fb33
ToothbrushPenguin <emil.k.m@hotmail.dk>
2024-10-04 11:29:07 +0200

    04-10-2024

---
+++ Pr14_BankAccountAndSorting/Øvelse3/Øvelse3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Øvelse3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Øvelse 3.1
+            int length = 5;
+
+            int i = 0;
+            while (i < length)
+            {
+                //do something
+                i++;
+            }
+
+            //Øvelse 3.2
+            int[] IntArray = { 3, 2, 4, 1 };
+            foreach (int number in IntArray)
+            {
+                //number = 1;
+            }
+
+        }
+    }
+}
