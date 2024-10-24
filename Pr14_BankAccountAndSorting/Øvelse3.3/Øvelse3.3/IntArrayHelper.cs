22dd3514d75c7f322c7e8a298d08ccc1c0b9aa7b
ToothbrushPenguin <emil.k.m@hotmail.dk>
2024-10-24 20:06:45 +0200

    exstra

---
+++ Pr14_BankAccountAndSorting/Øvelse3.3/Øvelse3.3/IntArrayHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Øvelse3._3
+{
+    internal class IntArrayHelper
+    {
+
+        public int[] SortAscending(int[] intArray)
+        {
+            int[] outArray = new int[intArray.Length];
+            for (int i = 0; intArray.Length)
+            {
+                if (i > )
+            }
+            return null;
+        }
+
+        public int[] SortAscendingAndReverse(int[] intArray)
+        {
+            return null;
+        }
+
+
+    }
+}
