539361efe8260332d010e198ee53b9fbeb2b13cb
ToothbrushPenguin <emil.k.m@hotmail.dk>
2024-10-30 11:35:51 +0100

    30-10-2024 adde exesises for the day

---
+++ Pr21_CustomExceptionHandling/Pr21_CustomExceptionHandling/TotallySafeLib/NegativeIndexOutOfRangeException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotallySafeLib
+{
+    public class NegativeIndexOutOfRangeException : Exception
+    {
+
+        public NegativeIndexOutOfRangeException()
+        {
+        
+        }
+
+        public NegativeIndexOutOfRangeException(string message) : base(message) 
+        { 
+
+        }
+        public NegativeIndexOutOfRangeException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+
+    }
+}
---
+++ Pr21_CustomExceptionHandling/Øvelse3/Øvelse3/FlowerSortRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Øvelse3
+{
+    internal class FlowerSortRepository
+    {
+        private List<FlowerSort> flowerSortsList;
+
+        FlowerSortRepository()
+        {
+            flowerSortsList = new List<FlowerSort>();
+        }
+
+        public void AddFlowerSort(string name,Image image, int productionTimeInDays, double halfLife, double size)
+        {
+            FlowerSort flowerSort = new FlowerSort(name, image, productionTimeInDays, halfLife, size);
+            
+            flowerSortsList.Add(flowerSort);
+        }
+    }
+}
