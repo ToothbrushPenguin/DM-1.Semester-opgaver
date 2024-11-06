50d6d16799a020e89141ca409b0d62dea9edc46d
ToothbrushPenguin <emil.k.m@hotmail.dk>
2024-11-06 15:32:52 +0100

    added the 06-11-2024 assignmen

---
+++ Pr24_WPFSimpleGUI/Øvelse3/WPFInteractiveGUI/WPFInteractiveGUI/PersonRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFInteractiveGUI
+{
+    public class PersonRepository
+    {
+        private List<Person> persons;
+
+        public int Count
+        {
+            get { return persons.Count; }
+        }
+
+        public PersonRepository()
+        {
+            persons = new List<Person>();
+        }
+
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+
+        public Person GetPersonAtIndex(int index)
+        {
+            Person personResultat = null;
+            if (index >= 0 && index < persons.Count)
+            {
+                personResultat = persons[index];
+            }
+
+            return personResultat;
+        }
+        public void RemovePersonAtIndex(int index)
+        {
+            if (index >= 0 && index < persons.Count)
+            {
+                persons.RemoveAt(index);
+            }
+        }
+        public void RemovePerson(Person person)
+        {
+            if (person != null)
+            {
+                persons.Remove(person);
+            }
+        }
+
+    }
+}
