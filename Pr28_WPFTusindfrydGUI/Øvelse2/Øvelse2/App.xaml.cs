01ee50e72a56edc6775df54c4cd3b58100638761
ToothbrushPenguin <emil.k.m@hotmail.dk>
2024-11-14 08:43:24 +0100

    added folder

---
+++ Pr28_WPFTusindfrydGUI/Øvelse2/Øvelse2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Øvelse2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
