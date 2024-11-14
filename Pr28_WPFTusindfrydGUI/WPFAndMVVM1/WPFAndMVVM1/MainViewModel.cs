89e655bb81cdec4717888f4079d7d8a3a0a3e3a3
snebsen <Sebastian03@live.dk>
2024-11-14 08:49:29 +0100

    Opgave 25

---
+++ Pr28_WPFTusindfrydGUI/WPFAndMVVM1/WPFAndMVVM1/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAndMVVM1
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public string MyLabelText { get; set; }
+
+        public string MyTextBoxText { get; set; }
+
+        public MainViewModel() 
+        {
+            MyLabelText = "Text not set yet";
+            MyTextBoxText = "Text not set yet";
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+    }
+}
---
+++ Pr28_WPFTusindfrydGUI/WPFAndMVVM1/WPFAndMVVM1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFAndMVVM1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        MainViewModel mvm = new MainViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = mvm;
+
+            Binding binding = new Binding("Text");
+            //binding.Mode = BindingMode.OneTime; Når man kører programmet vil den opdatere TB2 1 gang
+            //binding.Mode = BindingMode.OneWay; Den er kun den ene box der updatere den anden = TB3 -> TB2
+            //binding.Mode = BindingMode.OneWayToSource; Nu er det sourcen der bliver ændret fra TB2 -> TB3
+            binding.Mode = BindingMode.TwoWay; //Begge boxe kan ændre hinanden TB3 <-> TB2
+            binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            binding.Source = TB3;
+            TB2.SetBinding(TextBox.TextProperty, binding);
+
+            Binding bindingSlider = new Binding("Value");
+
+            bindingSlider.Mode = BindingMode.TwoWay;
+            bindingSlider.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            bindingSlider.Source = Slider;
+            TB4.SetBinding(TextBox.TextProperty, bindingSlider);
+
+            Binding labelBinding = new Binding();
+            labelBinding.UpdateSourceTrigger = UpdateSourceTrigger.Explicit;
+
+            labelBinding.Source = mvm.MyLabelText;
+            Label2.SetBinding(Label.ContentProperty, labelBinding);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mvm.MyLabelText = DateTime.Now.ToString();
+        }
+    }
+}
