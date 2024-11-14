dbc2caf2a78d93087a9b95e0ada4c8c37ffc84e5
snebsen <Sebastian03@live.dk>
2024-11-14 13:59:29 +0100

    Første forsøg champselect

---
+++ Pr28_WPFTusindfrydGUI/Øvelse2/Øvelse2/Models/Champ.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Øvelse2.Models
+{
+    public class Champ
+    {
+        public string Name { get; set; }
+        public Image Image { get; set; }
+        public string QSpell { get; set; }
+        public string WSpell { get; set; }
+        public string ESpell { get; set; }
+        public string RSpell { get; set; }
+    }
+}
+// Aatrox, Briar, Corki, Draven, Evelynn
---
+++ Pr28_WPFTusindfrydGUI/Øvelse2/Øvelse2/Models/ChampRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Øvelse2.Models
+{
+    public class ChampRepository
+    {
+        private List<Champ> champs = new List<Champ>();
+
+       
+    }
+}
+
