4d68de4ac3208ba72f84bb216660b40f4ff7f422
Søren Frederik Ploug <soren.ploug04@gmail.com>
2024-12-04 09:54:20 +0100

    Dagens Opgave Bonuses

---
+++ BonusApp/BonusApp/Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusApp
+{
+    public class Order
+    {
+        public BonusProvider Bonus;
+
+        private List<Product> products = new List<Product>();
+
+        public void AddProduct(Product p)
+        {
+            if (p != null)
+            {
+                products.Add(p);
+            }
+        }
+        public double GetValueOfProducts()
+        {
+            double valueOfProducts = 0.0;
+            foreach (Product p in products) 
+            {
+                valueOfProducts += p.Value;
+            }
+            return valueOfProducts;
+        }
+    
+
+        public double GetBonus()
+        {
+            return Bonus(GetValueOfProducts());
+        }
+
+
+        public double GetTotalPrice()
+        {
+            return GetValueOfProducts() - GetBonus();
+        }
+    }
+}
---
+++ BonusApp/BonusApp/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusApp
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+
+        /*public Product(string name, double value)
+        {
+            Name = name;
+            Value = value;
+        }
+        */
+
+    }
+
+    
+
+}
